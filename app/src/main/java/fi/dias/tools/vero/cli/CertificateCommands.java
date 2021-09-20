7276a03bc5d53b8684190b068aec73fcb6f36732
Jaakko Suutarla <jaakko@tomorrow.fi>
2021-09-20 09:31:25 +0300

    Release 1.0.0

--- app/src/main/java/fi/dias/tools/vero/cli/CertificateCommands.java
+++ app/src/main/java/fi/dias/tools/vero/cli/CertificateCommands.java
@@ -3,7 +3,7 @@ package fi.dias.tools.vero.cli;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "certificate",
-        version = {"certificate: 1.0.0-beta1",
+        version = {"certificate: 1.0.0",
                    "JVM: ${java.version} (${java.vendor} ${java.vm.name} ${java.vm.version})",
                    "OS: ${os.name} ${os.version} ${os.arch}"},
         subcommands = {NewCertificateCommand.class, RenewCertificateCommand.class},
